a9417ca60c9881d8ab5e9c49b42156aa64d9bd58
Algirdas Cernevicius <AlgirdasCernevicius@coherentsolutions.com>
2022-03-19 12:54:16 +0200

    UserSettings.cs implementation

---
+++ XMLFileHandler/XMLFileHandler/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using XMLFileHandler;
+
+Console.WriteLine("Hello, World!");
+var userAlex = new UserSettings("alex", "10", "20", "400", "200");
+userAlex.WriteToXml();
