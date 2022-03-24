65d1aa57d35131c48a4c150c61b25f921bffebd0
Algirdas Cernevicius <AlgirdasCernevicius@coherentsolutions.com>
2022-03-24 12:41:48 +0200

    Initial project structure

---
+++ Logger/Logger/Logger.cs
@@ -0,0 +1,19 @@
+﻿namespace Logger
+{
+    public class Logger
+    {
+        //TODO create a list of listeners...
+        private List<object> _lesteners = new List<object>();
+        //or just iterate with foreach
+
+        public Logger()
+        {
+
+        }
+        //or is it Track(object obj)??
+        public void Log(object obj)
+        {
+
+        }
+    }
+}
