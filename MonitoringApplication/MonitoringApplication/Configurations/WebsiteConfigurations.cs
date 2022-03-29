76b567385c64405552aab2081b736baff7f89661
Algirdas Cernevicius <AlgirdasCernevicius@coherentsolutions.com>
2022-03-29 08:50:24 +0300

    Initial commit

---
+++ MonitoringApplication/MonitoringApplication/Configurations/WebsiteConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringApplication.Configurations
+{
+    internal class WebsiteConfigurations
+    {
+        public int CheckInterval { get; set; }
+        public int ResponseTime { get; set; } //or MaxResponseTime
+        public string PageUrl { get; set; }
+        public string AdminEmail { get; set; }
+
+    }
+}
--- MonitoringApplication/MonitoringApplication/Program.cs
+++ MonitoringApplication/MonitoringApplication/Program.cs
@@ -1,2 +1,15 @@
 ﻿using Microsoft.Extensions.Logging.Console;
+using System.Net;
 
+var url = "https://docs.microsoft.com/en-us/dotnet/";
+
+var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+
+//runing check with exp: for() loop
+var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+{
+    var result = streamReader.ReadToEnd();
+}
+//if 
+Console.WriteLine(httpResponse.StatusCode);
